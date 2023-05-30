7bf2f07698fcb3faf90471d7ca6c0f35284370f5
Adilsonjvr <adilsonjvr@hotmail.com>
2023-05-30 13:20:55 -0300

    Primeiras modificações

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        
+        int resultadoSoma = calculadora.somar(3, 4);
+        System.out.println("Resultado da soma: " + resultadoSoma);
+        
+        int resultadoMultiplicacao = calculadora.multiplicar(5, 2);
+        System.out.println("Resultado da multiplicação: " + resultadoMultiplicacao);
     }
 }
